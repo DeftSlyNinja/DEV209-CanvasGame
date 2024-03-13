cd9e82ff66648588593e26b2f7d2f3dd6e2fed88
DeftSlyNinja <christopher_masson@hotmail.com>
2024-03-13 16:08:26 -0700

    added images and sound effects and defined basics

--- main.js
+++ main.js
@@ -1,8 +1,95 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+// Canvas & Context definition
 
-    document.getElementById("button1").addEventListener("click", function () {
+const canvas = document.createElement("canvas");
+const context = canvas.getContext("2d");
+canvas.width = 1366;
+canvas.height = 768;
+document.body.appendChild(canvas);
 
-        document.getElementById("textbox1").value = "success";
-    });
-});
+//Image definitions
 
+let bgReady = false;
+let bgImage = new Image();
+bgImage.onload = function () {
+    bgReady = true;
+};
+bgImage.src = "images/RPG Background.png"
+
+let zombieReady = false;
+let zombieImage = new Image();
+zombieImage.onload = function () {
+    zombieReady = true;
+}
+zombieImage.src = "images/Zombie Sprite.png"
+
+let brainReady = false;
+let brainImage = new Image();
+brainImage.onload = function() {
+    brainReady = true;
+}
+brainImage.src = "images/brain_PNG36.png"
+
+let shooterReady = false;
+let shooterImage = new Image();
+shooterImage.onload = function() {
+    shooterReady = true;
+}
+shooterImage.src = "images/shooter.png"
+
+// Game Objects
+
+let zombie = {
+    speed: 0,
+    x: 0,
+    y: 0
+};
+
+let brain = {
+    x:0,
+    y:0
+};
+
+let shooter = {
+    speed: 0,
+    x: 0,
+    y: 0
+};
+
+// Functions
+
+let main = function() {
+    render();
+    requestAnimationFrame(main);
+};
+
+let render = function() {
+    if (bgReady) {
+        context.drawImage(bgImage, 0, 0)
+    }
+
+    if (zombieReady) {
+        context.drawImage(zombieImage, zombie.x, zombie.y);
+    }
+
+    if (brainReady) {
+        context.drawImage(brainImage, brain.x, brain.y, brainImage.width/24, brainImage.height/24);
+    }
+
+    if (shooterReady) {
+        context.drawImage(shooterImage, shooter.x, shooter.y, shooterImage.width/2, shooterImage.height/2);
+    }
+};
+
+let reset = function() {
+    zombie.x = canvas.width/4;
+    zombie.y = canvas.height/4;
+
+    brain.x = 32 + Math.random() * (canvas.width -96);
+    brain.y = 32 + (Math.random() * (canvas.height - 96));
+}
+
+// Main
+
+let then = Date.now();
+reset();
+main();
