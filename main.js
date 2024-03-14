5d9c3bc6b918440c9f45c98ee83f492eb59e5474
DeftSlyNinja <christopher_masson@hotmail.com>
2024-03-14 12:44:41 -0700

    Added sound effects and character controls

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Canvas & Context definition
+// Canvas & Context definition -------------------------------------
 
 const canvas = document.createElement("canvas");
 const context = canvas.getContext("2d");
@@ -6,7 +6,34 @@ canvas.width = 1366;
 canvas.height = 768;
 document.body.appendChild(canvas);
 
-//Image definitions
+// Sprite management ------------------------------------------------
+
+let counter = 0;
+let rows = 4;
+let cols = 4;
+
+let trackRight = 1;
+let trackLeft = 2;
+let trackUp = 0;
+let trackDown = 3;
+
+let spriteWidth = 256;
+let spriteHeight = 255;
+let width = spriteWidth / cols; 
+let height = spriteHeight / rows; 
+
+let curXFrame = 0;
+let frameCount = 4;
+
+let srcX = 0;  
+let srcY = 0;
+
+let left = false;
+let right = false;
+let up = false;
+let down = false;
+
+//Image & Sound definitions ------------------------------------------------
 
 let bgReady = false;
 let bgImage = new Image();
@@ -36,10 +63,14 @@ shooterImage.onload = function() {
 }
 shooterImage.src = "images/shooter.png"
 
-// Game Objects
+let crunch = "sounds/crunch.mp3";
+let shotgun = "sounds/shotgun.mp3";
+let soundEfx = document.getElementById("soundEfx");
+
+// Game Objects ----------------------------------------------------
 
 let zombie = {
-    speed: 0,
+    speed: 250,
     x: 0,
     y: 0
 };
@@ -55,10 +86,98 @@ let shooter = {
     y: 0
 };
 
-// Functions
+let brainsEaten = 0;
+
+// Event Listeners / Movement --------------------------------------
+
+let keysDown = {};
+
+addEventListener("keydown", function (e) {
+    console.log(e.keyCode + " down")
+    keysDown[e.keyCode] = true;
+}, false);
+
+addEventListener("keyup", function (e) {
+    console.log(e.keyCode + " up")
+    delete keysDown[e.keyCode];
+}, false);
+
+let update = function (modifier) {
+
+    left = false;
+    right = false;
+    up = false;
+    down = false;
+
+    if (38 in keysDown && zombie.y > 64) { // Player holding up
+        zombie.y -= zombie.speed * modifier;
+        up = true;
+    }
+    if (40 in keysDown && zombie.y < canvas.height - 128) { // Player holding down
+        zombie.y += zombie.speed * modifier;
+        down = true;
+    }
+    if (37 in keysDown && zombie.x > 64) { // Player holding left
+        zombie.x -= zombie.speed * modifier;
+        left = true;
+    }
+    if (39 in keysDown && zombie.x < canvas.width - 128) { // Player holding right
+        zombie.x += zombie.speed * modifier;
+        right = true;
+    }
+
+    if (counter == 12) {
+        curXFrame = ++curXFrame % frameCount; 
+        counter = 0;
+    } else {
+        counter++;
+    }
+
+    srcX = curXFrame * width;
+
+    if (left) {
+        srcY = trackLeft * height;
+    }
+
+    if (right) {
+        srcY = trackRight * height;
+    }
+
+    if (up) {
+        srcY = trackUp * height;
+    }
+
+    if (down) {
+        srcY = trackDown * height;
+    }
+
+    if (left == false && right == false && up == false && down == false) {
+        srcX = 0 * width;
+        srcY = 0 * height;
+    }
+
+    if (
+        zombie.x <= (brain.x + 24)
+        && brain.x <= (zombie.x + 24)
+        && zombie.y <= (brain.y + 24)
+        && brain.y <= (zombie.y + 48)
+    ) {
+        ++brainsEaten;
+        soundEfx.src = crunch;
+        soundEfx.play();       
+        reset();  
+    }
+
+};
+
+// Functions -------------------------------------------------------
 
 let main = function() {
+    let now = Date.now();
+    let delta = now - then;
+    update(delta / 1000);
     render();
+    then = now;
     requestAnimationFrame(main);
 };
 
@@ -67,28 +186,37 @@ let render = function() {
         context.drawImage(bgImage, 0, 0)
     }
 
-    if (zombieReady) {
-        context.drawImage(zombieImage, zombie.x, zombie.y);
-    }
-
     if (brainReady) {
         context.drawImage(brainImage, brain.x, brain.y, brainImage.width/24, brainImage.height/24);
     }
 
-    if (shooterReady) {
-        context.drawImage(shooterImage, shooter.x, shooter.y, shooterImage.width/2, shooterImage.height/2);
+    // if (shooterReady) {
+    //     context.drawImage(shooterImage, shooter.x, shooter.y, shooterImage.width/2, shooterImage.height/2);
+    // }
+
+    if (zombieReady) {
+         context.drawImage(zombieImage, srcX, srcY, width, height, zombie.x, zombie.y, width, height);
     }
+
+    // Score
+
+    context.fillStyle = "rgb(250, 250, 250)";
+    context.font = "24px Helvetica";
+    context.textAlign = "left";
+    context.textBaseline = "top";
+    context.fillText("Brains Eaten: " + brainsEaten, 32, 32);
+
 };
 
 let reset = function() {
-    zombie.x = canvas.width/4;
-    zombie.y = canvas.height/4;
+    zombie.x = canvas.width/2;
+    zombie.y = canvas.height/2;
 
-    brain.x = 32 + Math.random() * (canvas.width -96);
-    brain.y = 32 + (Math.random() * (canvas.height - 96));
+    brain.x = 64 + Math.random() * (canvas.width - 192);
+    brain.y = 64 + (Math.random() * (canvas.height - 192));
 }
 
-// Main
+// Main Loop -------------------------------------------------------
 
 let then = Date.now();
 reset();
